27461555be0041483776b444c288ed3e8b6e0427
Anyela Camargo Rodriguez <X992029@niab.com>
2019-03-21 10:20:51 +0000

    new file

--- scrapper/grantscrapper.R
+++ scrapper/grantscrapper.R
@@ -10,9 +10,9 @@ call <- read_html("bbsrc.html") %>%
   html_nodes("h3 a") %>%
   html_text()
 
-batch_titles <- batches %>%
-  html_nodes("href") %>%
-  html_text()
+#batch_titles <- batches %>%
+#  html_nodes("href") %>%
+#  html_text()
 
 
 url <- read_html("bbsrc.html") %>%
@@ -25,4 +25,5 @@ date <- read_html("bbsrc.html") %>%
   html_nodes("time") %>%
   html_text()
 
-calltable <- data.frame(callname = call, deadline = date, call_url = paste('https://www.bbsrc.ac.uk', url, sep=''))
+calltable <- data.frame(callname = call, deadline = date, call_url = 
+                          paste('https://www.bbsrc.ac.uk', url, sep=''))
