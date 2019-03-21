6605a603501627da225a8860bd4799b8dd7dc8ee
comptpbio <X992029@niab.com>
2019-03-21 11:24:17 +0000

    more links

--- build_site.R
+++ build_site.R
@@ -3,8 +3,11 @@
 #not our site because of other projects we were 
 #working on at the time. 
 setwd("/Anyela/repo/comptpbio.github.io")
+system("../../../phantomjs-2.1.1-windows/bin/phantomjs.exe nerc.js")
+system("../../../phantomjs-2.1.1-windows/bin/phantomjs.exe bbsrc.js")
 
 #render your sweet site. 
+rmarkdown::render('grantsearchlist.Rmd',output_file='grantsearchlist.html')
 rmarkdown::render_site()
 
 
