95c78b03a70a39c093143b884e1bfe9451f92191
comptpbio <X992029@niab.com>
2019-06-14 15:17:50 +0100

    new links

--- build_site.R
+++ build_site.R
@@ -4,7 +4,7 @@
 setwd("/Anyela/repo/comptpbio.github.io")
 system("../../phantomjs-2.1.1-windows/bin/phantomjs.exe scrapper/nerc.js")
 system("../../phantomjs-2.1.1-windows/bin/phantomjs.exe scrapper/bbsrc.js")
-
+system("cp bbsrc.html scrapper")
 #render your sweet site. 
 rmarkdown::render('grantsearchlist.Rmd',output_file='grantsearchlist.html')
 rmarkdown::render_site()
